63ae3d389b8b6d3221baa81ad1ce43ed6d72b527
zhangzhe211506344 <994941361@qq.com>
2017-10-29 01:36:29 +0800

    2

--- class/StaticData.h
+++ class/StaticData.h
@@ -12,7 +12,14 @@
 
 #if !defined(_STATICDATA_H)
 #define _STATICDATA_H
-
+#define STATIC_DATA_FILENAME "static_data.plist"
+#define STATIC_DATA_STRING(key) StaticData::sharedStaticData()->stringValueFromKey(key)
+#define STATIC_DATA_INT(key) StaticData::sharedStaticData()->intValueFromKey(key)
+#define STATIC_DATA_FlOAT(key) StaticData::sharedStaticData()->floatValueFromKey(key)
+#define STATIC_DATA_BOOL(key) StaticData::sharedStaticData()->booleanFromKey(key)
+#define STATIC_DATA_POINT(key) StaticData::sharedStaticData()->pointFromKey(key)
+#define STATIC_DATA_RECT(key) StaticData::sharedStaticData()->rectFromKey(key)
+#define STATIC_DATA_SIZE(key) StaticData::sharedStaticData()->sizeFromKey(key)
 #using <mscorlib.dll>
 
 class StaticData {
